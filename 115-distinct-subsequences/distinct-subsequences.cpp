745650bbe064dc851700f3f878f1896ebc6be3ba
Sudhanshu Chaubey <77341502+Sudhanshu-273@users.noreply.github.com>
2024-01-31 01:31:09 +0530

    Time: 22 ms (77.85%) | Memory: 19.7 MB (49.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -7,7 +7,7 @@ public:
         if (i < 0 && j < 0) {
             return 1;
         }
-        if(i < 0 && j >= 0) {
+        if (i < 0 && j >= 0) {
             return 0;
         }
 
@@ -18,10 +18,11 @@ public:
         int x = 0, y = 0, z = 0;
         if (s[i] == t[j]) {
             x = solve(s, t, i - 1, j - 1, dp);
-            y = solve(s, t, i - 1, j, dp);
-        } else {
-            z = solve(s, t, i - 1, j, dp);
         }
+            // y = solve(s, t, i - 1, j, dp);
+        // } else {
+            z = solve(s, t, i - 1, j, dp);
+        // }
 
         return dp[i][j] = x + y + z;
     }
@@ -30,6 +31,24 @@ public:
         int n = s.length(), m = t.length();
 
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        // for (int i = 0; i <= n; ++i) {
+        //     dp[i][0] = 1;
+        // }
+
+        // for (int i = 1; i <= n; ++i) {
+        //     for (int j = 1; j <= m; ++j) {
+        //         long val = 0;
+        //         if (s[i - 1] == t[j - 1]) {
+        //             val += dp[i - 1][j - 1];
+        //         }
+        //         val += dp[i - 1][j];
+
+        //         dp[i][j] = val;
+        //     }
+        // }
+
+        // return dp[n][m];
 
         return solve(s, t, n - 1, m - 1, dp);
     }
