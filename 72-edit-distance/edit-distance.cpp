68aaf57ae5767aa999af28b930417df27d409c24
Sudhanshu Chaubey <77341502+Sudhanshu-273@users.noreply.github.com>
2024-02-03 20:22:22 +0530

    Time: 3 ms (96.48%) | Memory: 9.6 MB (41.62%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-
-    int solve(string &s, string &t, int i, int j, vector<vector<int>> &dp) {
+    int solve(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
         // base
 
-        if(j < 0)
+        if (j < 0)
             return i + 1;
-        if(i < 0)
+        if (i < 0)
             return j + 1;
 
-        if(dp[i][j] != -1) {
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-
         //
         int a = 1e6, b = 1e6, c = 1e6, d = 1e6;
-        if(s[i] == t[j]) {
+        if (s[i] == t[j]) {
             a = solve(s, t, i - 1, j - 1, dp);
         } else {
             // insert
@@ -28,33 +26,30 @@ public:
             d = 1 + solve(s, t, i - 1, j - 1, dp);
         }
 
-        return dp[i][j] =min(a, min(b, min(c, d)));
+        return dp[i][j] = min(a, min(b, min(c, d)));
     }
 
     int minDistance(string s, string t) {
         int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
-        for(int i = 0; i <= n; ++i) {
-            dp[i][0] = i + 1;
-        }
-        for(int i = 0; i <= m; ++i) {
-            dp[0][i] = i + 1;
+        for (int j = 0; j <= m; ++j) {
+            prev[j] = j;
         }
 
-        for(int i = 1; i <= n; ++i) {
-            for(int j = 1; j <= m; ++j) {
-                if(s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+        for (int i = 1; i <= n; ++i) {
+            curr[0] = i;
+            for (int j = 1; j <= m; ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    curr[j] = prev[j - 1];
                 } else {
-                    dp[i][j] = 1 + dp[i][j - 1];
-                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j]);
-                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j - 1]);
+                    curr[j] = 1 + min(curr[j - 1], min(prev[j], prev[j - 1]));
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m] - 1;
+        return prev[m];
         // return solve(s, t, n - 1, m - 1, dp);
     }
 };
