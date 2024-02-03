c99f6a16f668f7e8b9e25b08fd5483b9fa1a5fe0
Sudhanshu Chaubey <77341502+Sudhanshu-273@users.noreply.github.com>
2024-02-03 20:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (27.28%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -33,10 +33,28 @@ public:
 
     int minDistance(string s, string t) {
         int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        if(m == 0) {
-            return n; 
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+
+        for(int i = 0; i <= n; ++i) {
+            dp[i][0] = i + 1;
         }
-        return solve(s, t, n - 1, m - 1, dp);
+        for(int i = 0; i <= m; ++i) {
+            dp[0][i] = i + 1;
+        }
+
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j <= m; ++j) {
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + dp[i][j - 1];
+                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j]);
+                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m] - 1;
+        // return solve(s, t, n - 1, m - 1, dp);
     }
 };
