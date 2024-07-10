620fea17f153a09d6dd8fc1e2b4a0f5b41e93373
Sudhanshu Chaubey <77341502+Sudhanshu-273@users.noreply.github.com>
2024-07-10 13:52:20 +0530

    Time: 15 ms (49.89%) | Memory: 15.7 MB (7.46%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,22 +1,14 @@
-#define ff first
-#define ss second
 class Solution {
 public:
     vector<int> twoSum(vector<int>& v, int target) {
-        int s = 0, e = v.size() - 1;
-        vector<pair<int, int>> vp(v.size());
+        unordered_map<int, int> mp;
+        for(int i = 0; i < v.size(); ++i) mp[v[i]] = i;
         for(int i = 0; i < v.size(); ++i) {
-            vp[i] = {v[i], i};
+            if(mp.find(target - v[i]) != mp.end()) {
+                if(mp[target-v[i]] != i)
+                    return {i, mp[target - v[i]]};
+            }
         }
-        sort(vp.begin(), vp.end());
-        while(e > s) {
-            if(vp[s].ff + vp[e].ff == target) {
-                return {min(vp[s].ss, vp[e].ss), max(vp[s].ss, vp[e].ss)};
-            } else if(vp[s].ff + vp[e].ff > target) {
-                --e;
-            } else ++s;
-        }
-
         return {};
     }
 };
